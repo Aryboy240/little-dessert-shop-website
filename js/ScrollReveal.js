a844aed33da716d01530643bad25cbf5328b865b
Aryan :) <94476184+Aryboy240@users.noreply.github.com>
2024-10-11 12:33:49 +0100

    Adding the website code
    I didn't add the images or videos

---
+++ js/ScrollReveal.js
@@ -0,0 +1,88 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 1000,
+    reset: true
+});
+
+
+// Global
+sr.reveal('.container', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.text-box', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.cards', {
+    duration: 1000,
+    interval: 100
+});
+
+
+// Home
+sr.reveal('.slidecon', {
+    duration: 1000,
+    interval: 100
+});
+
+
+// Store
+sr.reveal('#map', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.contactInfo', {
+    duration: 1000,
+    delay: 100,
+    interval: 100
+});
+
+sr.reveal('.contactForm', {
+    duration: 1000,
+    delay: 300,
+    interval: 100
+});
+
+
+// Menu
+sr.reveal('.top-vid', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.Menu-card-body', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.rainbow-con', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.product-card', {
+    duration: 1000,
+    interval: 100
+});
+
+
+// Feedback
+sr.reveal('.testimonial-heading span', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.testimonial-heading h1', {
+    duration: 1000,
+    interval: 100
+});
+
+sr.reveal('.testimonial-box', {
+    duration: 1000,
+    interval: 100
+});
